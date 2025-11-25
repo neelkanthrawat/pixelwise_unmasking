49fbe2569ad7c33a9459c6f825024bfcb1ad62aa
Neelkanth Rawat <neelkanthrawat3992@gmail.com>
2025-11-25 20:11:15 +0100

    add code to generate dataloaders for masked mnist

--- scripts/data/dataloaders_related.py
+++ scripts/data/dataloaders_related.py
@@ -1,5 +1,8 @@
 import torch
 from torchvision import transforms, datasets
+from torch.utils.data import Dataset
+import numpy as np
+from typing import Literal
 
 
 class AddGaussianNoise(object):
@@ -49,5 +52,132 @@ def get_mnist_dataloaders(batchsize, pixelwidth, digit=None, zero_one_range=Fals
     # Create data loaders for batching and shuffling
     train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batchsize, shuffle=True)
     test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batchsize, shuffle=True)
+
+    return train_loader, test_loader
+
+
+### for conditional_encoder/decoder related thing
+#NOTE: FLATTEN THE MASK before concatenation in the encoder:
+# cond = mask.view(mask.shape[0], -1)
+# x = encoder(masked_image, cond)
+
+class MaskedMNIST(Dataset):
+    def __init__(self, base_dataset, train=True, 
+                 mask_type:Literal['square', 'random', 'checkerboard_pattern']='square',
+                 num_pixel_masked_random_case:int = 100):
+        """
+        Initialize MNIST with randomply masked areas in rectengular format
+
+        Parameter:
+            root (str): Root directory of dataset where ``MNIST/processed/training.pt``
+                        and  ``MNIST/processed/test.pt`` will be saved.
+            train (bool):   If True, creates dataset from ``training.pt``,
+                            otherwise from ``test.pt``.
+        """
+        if base_dataset is None:
+            raise ValueError("base_dataset must be provided")
+        self.base_dataset = base_dataset
+        self.mask_fns = {
+                        "square": self.apply_square_mask,
+                        "random": self.apply_random_mask,
+                        "checkerboard_pattern": ...,
+                    }
+        self.mask_type = mask_type
+        if self.mask_type == 'random':
+            self.num_pixel_masked_random = num_pixel_masked_random_case
+
+
+    def __len__(self):
+        """
+        Return the number of samples in dataset.
+        """
+        return len(self.base_dataset)
+
+    def __getitem__(self, idx):
+        """
+        Fetch a sample for a given index, apply a random squared mask, and return both masked and original images.
+        """
+        image = self.base_dataset[idx] # imag, label, we dont care about label
+
+        # Generate a random square mask
+        mask_fn = self.mask_fns[self.mask_type]
+        masked_image, mask = mask_fn(image)
+
+        # Return the masked image, original image, and label AS WELL AS THE INDICES OF THE MASK POSITION ( we need it while sending in the condition)
+        return image, masked_image, mask, label # orignal_image_which_decoder_should_reconstruct, masked_image_fed_as_input_to_encoder, condition (used in encoder and decoder)
+
+    def apply_square_mask(self, image):
+        """
+        Apply a random square mask to the image.
+        """
+        im_size = image.size(1)
+        # Random mask size between 1/4 to 1/2 of image size
+        mask_size = np.random.randint(im_size // 4, im_size // 2)
+        top = np.random.randint(0, im_size - mask_size)
+        left = np.random.randint(0, im_size - mask_size)
+
+        mask = torch.ones_like(image)
+        mask[:, top : top + mask_size, left : left + mask_size] = 0  # Apply mask
+
+        masked_image = image.clone()  # Clone to not modify the original image
+        masked_image[:, top : top + mask_size, left : left + mask_size] = -1
+
+        return masked_image, mask
+    
+    def apply_random_mask(self, image):
+        """  
+        Apply mask at random position of image
+        """
+        im_size = image.size(1)
+
+        mask = torch.ones_like(image)
+        masked_image = image.clone()
+
+        ys = np.random.randint(0, im_size, size=self.num_pixel_masked_random)
+        xs = np.random.randint(0, im_size, size=self.num_pixel_masked_random)
+
+        mask[:, ys, xs] = 0
+        masked_image[:, ys, xs] = -1 # some negative value (super low)
+        
+        return masked_image, mask
+    
+
+
+def get_masked_mnist_dataloaders(batchsize, pixelwidth, digit=None, zero_one_range=False):
+
+    if not zero_one_range:
+        transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Resize((pixelwidth, pixelwidth)),
+            transforms.Normalize((0.1307,), (0.3081,)),
+            AddGaussianNoise(mean=0.0, std=0.01),
+            transforms.Lambda(lambda x: x.view(-1))
+        ])
+    else:
+        transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Resize((pixelwidth, pixelwidth)),
+            transforms.Lambda(lambda x: x.view(-1))
+        ])
+
+    # Base MNIST
+    train_base = datasets.MNIST(root='./data', train=True, download=True, transform=transform)
+    test_base  = datasets.MNIST(root='./data', train=False, download=True, transform=transform)
+
+    # Digit filter
+    if digit is not None:
+        train_idx = [i for i, label in enumerate(train_base.targets) if label == digit]
+        test_idx  = [i for i, label in enumerate(test_base.targets) if label == digit]
+
+        train_base = torch.utils.data.Subset(train_base, train_idx)
+        test_base  = torch.utils.data.Subset(test_base, test_idx)
+
+    # Wrap in paired dataset
+    train_dataset = MaskedMNIST(train_base)
+    test_dataset  = MaskedMNIST(test_base)
+
+    # DataLoaders
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batchsize, shuffle=True)
+    test_loader  = torch.utils.data.DataLoader(test_dataset, batch_size=batchsize, shuffle=True)
 
     return train_loader, test_loader
